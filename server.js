7cfc10aa4f45f249eab09021609c6def55626955
eriksvetlik <svetlik.erik@gmail.com>
2022-02-10 08:41:58 -0600

    updated starter code

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers');
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+require("dotenv").config({ path: require("find-config")(".env") });
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -26,5 +27,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening at port 3001"));
 });
