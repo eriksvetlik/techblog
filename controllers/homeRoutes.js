140384e764a6b3ed496e10759868ab1770bcbbe2
eriksvetlik <svetlik.erik@gmail.com>
2022-02-10 15:45:51 -0600

    overhauled code using mini project as starter

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,76 +1,81 @@
 const router = require("express").Router();
-const { Post, User } = require("../models");
+const { Project, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-// GET all galleries for homepage
 router.get("/", async (req, res) => {
   try {
-    const postData = await Post.findAll({
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
       include: [
         {
           model: User,
-          attributes: ["username"],
+          attributes: ["name"],
         },
       ],
     });
 
-    const posts = postData.map((post) => post.get({ plain: true }));
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
 
+    // Pass serialized data and session flag into template
     res.render("homepage", {
-      posts,
-      loggedIn: req.session.loggedIn,
+      projects,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
-// // GET one gallery
-// router.get("/gallery/:id", async (req, res) => {
-//   try {
-//     const dbGalleryData = await Gallery.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Painting,
-//           attributes: [
-//             "id",
-//             "title",
-//             "artist",
-//             "exhibition_date",
-//             "filename",
-//             "description",
-//           ],
-//         },
-//       ],
-//     });
+router.get("/project/:id", async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-//     const gallery = dbGalleryData.get({ plain: true });
-//     res.render("gallery", { gallery, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+    const project = projectData.get({ plain: true });
 
-// // GET one painting
-// router.get("/painting/:id", async (req, res) => {
-//   try {
-//     const dbPaintingData = await Painting.findByPk(req.params.id);
+    res.render("project", {
+      ...project,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//     const painting = dbPaintingData.get({ plain: true });
-//     res.render("painting", { painting, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+// Use withAuth middleware to prevent access to route
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Project }],
+    });
 
-// Login route
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
     return;
   }
+
   res.render("login");
 });
 
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const session = require("express-session");
-const routes = require("./controllers");
 const exphbs = require("express-handlebars");
+const express = require("express");
+const helpers = require("./utils/helpers");
 const path = require("path");
-const hbs = exphbs.create({});
-require("dotenv").config({ path: require("find-config")(".env") });
+const routes = require("./controllers");
+const session = require("express-session");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SESSION_SECRET,
