c24ba7e411f0717dfa60a726c33005a51a2db2cf
eriksvetlik <svetlik.erik@gmail.com>
2022-02-11 15:48:26 -0600

    added comment model and view

--- models/index.js
+++ models/index.js
@@ -10,4 +10,13 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
 module.exports = { User, Post };
---
+++ public/js/comment.js
@@ -0,0 +1,25 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const description = document.querySelector("#comment-desc").value.trim();
+
+  if (description) {
+    const response = await fetch(`/api/comments`, {
+      method: "POST",
+      body: JSON.stringify({ description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/post");
+    } else {
+      alert("Failed to create comment");
+    }
+  }
+};
+
+document
+  .querySelector(".new-comment-form")
+  .addEventListener("submit", newFormHandler);
