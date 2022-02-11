630ab2eb503a2f85b7aac70f52681cfc2898ba59
eriksvetlik <svetlik.erik@gmail.com>
2022-02-11 16:34:25 -0600

    added comment seed data

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
 User.hasMany(Post, {
   foreignKey: "user_id",
@@ -19,4 +20,4 @@ Comment.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
